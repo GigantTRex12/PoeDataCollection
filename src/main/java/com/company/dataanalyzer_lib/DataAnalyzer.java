fb495237285a8eef9df2062763b2dc230d27edc3
GigantTRex12 <GigantTRex12@gmx.de>
2026-05-24 07:17:05 +0200

    boss drop analyzer

--- src/main/java/com/company/dataanalyzer_lib/DataAnalyzer.java
+++ src/main/java/com/company/dataanalyzer_lib/DataAnalyzer.java
@@ -1,6 +1,7 @@
 package com.company.dataanalyzer_lib;
 
 import analyzer.BaseDataAnalyzer;
+import analyzer.GroupingDefinition;
 import com.company.dataset_lib.DataSet;
 
 import java.util.List;
@@ -8,7 +9,12 @@ import java.util.function.Supplier;
 
 public abstract class DataAnalyzer<T extends DataSet> extends BaseDataAnalyzer<T> {
 
-    public DataAnalyzer(Supplier<List<T>> dataSupplier) {
+    protected final GroupingDefinition<T> strategyGrouper;
+    protected final GroupingDefinition<T> leagueGrouper;
+
+    protected DataAnalyzer(Supplier<List<T>> dataSupplier) {
+        strategyGrouper = new GroupingDefinition<>("strategy", DataSet::getStrategy);
+        leagueGrouper = new GroupingDefinition<>("league", d -> d.getStrategy().getLeague());
         super(dataSupplier.get());
     }
 
--- src/main/java/com/company/dataanalyzer_lib/analyzers/BossDropDataAnalyzer.java
+++ src/main/java/com/company/dataanalyzer_lib/analyzers/BossDropDataAnalyzer.java
@@ -5,8 +5,14 @@ import analyzer.Question;
 import com.company.api.DbReader;
 import com.company.dataanalyzer_lib.DataAnalyzer;
 import com.company.dataset_lib.datasets.BossDropDataSet;
+import com.company.datasets.other.loot.Loot;
+import com.company.datasets.other.loot.LootType;
+import com.company.datasets.other.loot.StackableLoot;
+import com.company.utils.Counter;
+import com.company.utils.Utils;
 
 import java.util.List;
+import java.util.Map;
 
 public class BossDropDataAnalyzer extends DataAnalyzer<BossDropDataSet> {
 
@@ -20,10 +26,32 @@ public class BossDropDataAnalyzer extends DataAnalyzer<BossDropDataSet> {
         return List.of(
                 Question.ask("boss_unique", c)
                         .forcedGrouping(BossDropDataSet::lowerCaseBossname)
-                        .groupings(new GroupingDefinition<>("league", d -> d.getStrategy().getLeague()))
+                        .groupings(leagueGrouper, new GroupingDefinition<>("witness", BossDropDataSet::isWitnessed))
                         .evaluator(d -> d.getGuaranteedDrop().getName(), WILSON_CONFIDENCE)
                         .conditionAll(d -> d.getGuaranteedDrop() != null)
+                        .build(),
+                Question.ask("extra_drops", c)
+                        .forcedGrouping(BossDropDataSet::lowerCaseBossname)
+                        .groupings(leagueGrouper)
+                        .evaluator(BossDropDataSet::getExtraDrops, this::countExtraDrops)
                         .build()
         );
     }
+
+    private void countExtraDrops(List<List<Loot>> drops) {
+        Counter<String> lootCounter = new Counter<>();
+        for (List<Loot> dropList : drops) {
+            for (Loot loot : dropList) {
+                if (loot instanceof StackableLoot l) lootCounter.add(l.getName(), l.getStackSize());
+                else if (loot.getType() == LootType.BOSS_UNIQUE_ITEM) lootCounter.add(loot.getName());
+                else lootCounter.add(loot.getType().name());
+            }
+        }
+
+        int size = drops.size();
+        for (Map.Entry<String, Integer> entry : lootCounter.entrySet()) {
+            IO.println(entry.getKey() + ": " + entry.getValue() + "/" + size + " (" + Utils.toPercentage(entry.getValue(), size, 2) + ")");
+        }
+    }
+
 }
